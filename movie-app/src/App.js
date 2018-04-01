dca74c04373eb8d4bdc977a6ee3f758b1c7e7b21
Limjunghwa <nin0405@naver.com>
2018-04-02 00:28:05 +0900

    첫 커밋

---
+++ movie-app/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      hello lim?
+      </div>
+    );
+  }
+}
+
+export default App;
