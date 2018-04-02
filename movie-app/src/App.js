01d4d2582e42a2ba74c8b2bfd4acbfd5c75ffaaf
Limjunghwa <nin0405@naver.com>
2018-04-02 14:33:30 +0900

    Loading 기능 추가
    5초 전 Loading 문구 추가 / 5초 후 리스트 보이기

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -8,50 +8,43 @@ class App extends Component {
 
  state={
    greeting:"hello?",
-   movies : [
-     {
-       title:"Matrix",
-       poster:"https://i.ytimg.com/vi/mSWDTG3I-dQ/maxresdefault.jpg"
-     },
-     {
-       title:"Full Metal Jacket",
-       poster:"https://ww3.sinaimg.cn/large/87c01ec7gy1fhg19uu0o8j208c0bo75w.jpg"
-     },
-     {
-       title:"Oldboy",
-       poster:"https://images-na.ssl-images-amazon.com/images/I/91wPAjXfJaL._SL1500_.jpg"
-     },
-     {
-       title:"Star Wars",
-       poster:"https://t1.daumcdn.net/thumb/R1280x0/?fname=http://t1.daumcdn.net/brunch/service/user/RKc/image/AHaBhR3QEffn-YOYsIkdP_nMnD4.jpg"
-     }
-
-   ]
  }
  componentDidMount(){
    setTimeout(()=>{
      this.setState({
-       greeting:"hellozzzz",
-       movies:[
-         ...this.state.movies,
+       movies :[
          {
-          title:"limjunghwa",
-          poster: "https://images-na.ssl-images-amazon.com/images/I/91wPAjXfJaL._SL1500_.jpg"
+           title:"Matrix",
+           poster:"https://i.ytimg.com/vi/mSWDTG3I-dQ/maxresdefault.jpg"
+         },
+         {
+           title:"Full Metal Jacket",
+           poster:"https://ww3.sinaimg.cn/large/87c01ec7gy1fhg19uu0o8j208c0bo75w.jpg"
+         },
+         {
+           title:"Oldboy",
+           poster:"https://images-na.ssl-images-amazon.com/images/I/91wPAjXfJaL._SL1500_.jpg"
+         },
+         {
+           title:"Star Wars",
+           poster:"https://t1.daumcdn.net/thumb/R1280x0/?fname=http://t1.daumcdn.net/brunch/service/user/RKc/image/AHaBhR3QEffn-YOYsIkdP_nMnD4.jpg"
          }
+
        ]
      })
    },5000)
  }
+ _renderMovies = () =>{
+   const movies = this.state.movies.map((movie,index) => {
+     return <Movie title={movie.title} poster={movie.poster} key={index}/>
+   })
+   return movies;
+ }
   render() {
     return (
       <div className="App">
-      {this.state.greeting}
-      {
-
-      this.state.movies.map((movie,index) => {
-        return <Movie title={movie.title} poster={movie.poster} key={index}/>
-      })
-    }
+        {this.state.movies ? this._renderMovies() : "Loading"}
+      
       </div>
     );
   }
