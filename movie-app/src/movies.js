3b3302b24af8d336ab895eb39938239d90660716
Limjunghwa <nin0405@naver.com>
2018-04-02 18:25:34 +0900

    smart 컴포넌트 -> dumb 컴포넌트로 변경
    movie&moviePoster 클래스를 function movie&moviePoster로 변경

--- movie-app/src/movies.js
+++ movie-app/src/movies.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './movies.css';
 import PropTypes from 'prop-types';
-
+/*
 class Movie extends Component{
   static propTypes={
     title:PropTypes.string.isRequired,
@@ -17,7 +17,22 @@ class Movie extends Component{
     );
   }
 }
+*/
+function Movie({title,poster}){
+  return(
+    <div>
+      <h1>{title}</h1>
+      <MoviePoster poster={poster} />
+    </div>
+  );
+}
 
+
+Movie.propTypes ={
+  title:PropTypes.string.isRequired,
+  poster:PropTypes.string.isRequired
+}
+/*
 class MoviePoster extends Component{
 
   render(){
@@ -26,4 +41,15 @@ class MoviePoster extends Component{
     );
   }
 }
+*/
+function MoviePoster({poster}){
+  return(
+    <img src={poster} alt="Movie Poster"/>
+  );
+}
+
+MoviePoster.propTypes ={
+  poster:PropTypes.string.isRequired
+}
+
 export default Movie;
